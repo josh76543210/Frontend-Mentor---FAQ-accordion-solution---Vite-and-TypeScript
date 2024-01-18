8203e94976b2bca2e7db9ae799aa1f6dd79b0d00
Josh <joshmm16@gmail.com>
2024-01-17 16:37:05 -0800

    Add toggle functionality with TypeScript;

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,7 +2,39 @@ import "../css/reset.css";
 import "../css/style.css";
 import "../css/queries.css";
 
-import test from "./test";
+/////////////////////
+// Elements
+const faqEls: NodeListOf<HTMLObjectElement> =
+  document.querySelectorAll(".card__faq");
 
-console.log("Hello TypeScript");
-console.log(test("Test"));
+console.log(faqEls);
+
+/////////////////////
+// Event Listeners
+// add click event for each faq element
+faqEls.forEach((el) => {
+  el.addEventListener("click", handleFAQ);
+});
+
+/////////////////////
+// Funtions
+/**
+ * This functions opens or closes the faq answers
+ */
+function handleFAQ(e: Event) {
+  // get question element
+  const questionEl: HTMLParagraphElement | null = (<HTMLElement>(
+    e.target
+  )).closest(".faq__question");
+
+  // check if question was clicked
+  if (questionEl === null) return;
+
+  // get faq element
+  const faqEl: HTMLParagraphElement | null = (<HTMLElement>e.target).closest(
+    ".card__faq"
+  );
+
+  // toggle faq element
+  faqEl?.classList.toggle("open");
+}
